d9c99ce330b2c8f55d2caf986d71579a4de91350
hanspeterdietiker <126719678+hanspeterdietiker@users.noreply.github.com>
2023-03-23 14:11:56 -0300

    Add files via upload

---
+++ src- week2/HowUseFor.java
@@ -0,0 +1,14 @@
+
+public class HowUseFor {
+	public static void main(String[] args) {
+		
+		// 1- Declarar uma variavel 2- Dar uma Condicao 3- Diminuir(--=-1) ou aumentar (++=+1) 
+		
+		for (int c = 15; c >= 1; c--) {
+			
+		//Neste caso ira diminuir(c--) o 15(int c) até o 1(c>=1)
+			
+			System.out.print(c + " ");
+		}
+	}
+}
