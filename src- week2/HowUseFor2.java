3cc7c645da6dcbd0c65802767df2d543e5ecda76
hanspeterdietiker <126719678+hanspeterdietiker@users.noreply.github.com>
2023-03-23 14:28:48 -0300

    Add files via upload

---
+++ src- week2/HowUseFor2.java
@@ -0,0 +1,12 @@
+
+public class HowUseFor2 {
+	public static void main(String[] args) {
+
+		String[] nomes = { "Joao", "Maria", "Carla" };
+
+		for (String nome : nomes) {
+			System.out.println(nome);
+
+		}
+	}
+}
